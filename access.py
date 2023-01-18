0a8b5c2b028ec7c8c637af2f740e4d86b97b6539
igorgaleisa101 <igorgaleisa101@gmail.com>
2023-01-18 22:47:51 +0200

    updted zaki's ip

--- access.py
+++ access.py
@@ -1,6 +1,6 @@
 WHITELIST = [
     '127.0.0.1',        # localhost, Don't remove.
-    '154.237.75.106',   # Zaki
+    '197.62.72.68',     # Zaki
     '149.56.240.156',   # Jian
     '45.33.77.221',     # John
 
